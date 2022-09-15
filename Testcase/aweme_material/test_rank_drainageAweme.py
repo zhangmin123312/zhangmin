24d8940ccfbd55b99431bc5e117aa878356f847f
尤江永 <11445183+you-jiangyong@user.noreply.gitee.com>
2022-09-15 11:07:40 +0800

    音乐库搜索优化 引流榜title调整

--- Testcase/aweme_material/test_rank_drainageAweme.py
+++ Testcase/aweme_material/test_rank_drainageAweme.py
@@ -16,7 +16,7 @@ class TestCase_Rank_DrainageAweme():
 
     @allure.story('验证引流视频榜遍历商品一级级分类返回的数据是否大于10条')
     @pytest.mark.parametrize('times', base.return_time_message_lastday())
-    @allure.title("引流视频榜日期:{times},类目：{product_type}")
+    @allure.title("引流视频榜日期:{times}")
     def test_rank_promotionAweme_big_type(self, get_token, get_host, times):
         para = f'day_type={times[0]}&order=play_inc&day={times[1]}&big_category=&first_category=&second_category=&order_by=desc&page=1&size=50'
         response = base().return_request(method="get", path=PathMessage.rank_DrainageAweme, data=para, tokens=get_token,hosts=get_host, )
