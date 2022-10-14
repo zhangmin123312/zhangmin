9dd3e89050a3ecc159708f1dd3b47ed4adf364e8
尤江永 <11445183+you-jiangyong@user.noreply.gitee.com>
2022-10-14 17:00:03 +0800

    bug优化

--- Common/Base.py
+++ Common/Base.py
@@ -17,10 +17,8 @@ from Config.Consts import user_agent
 from Config.path_config import PathMessage
 from Config.Consts import API_ENVIRONMENT,telephone,user_agent
 from Common.mylog import Mylog
-from Config.Consts import user_agent,webhook,secret,at_phone
-import hmac
-import base64
-import urllib.parse
+
+
 
 class base():
 
@@ -29,25 +27,23 @@ class base():
         """复制文件"""
         shutil.copyfile(source_file,targrt_file)
 
-    @staticmethod
-    def dingding_notic():
-        # 钉钉通知
-        timestamp = str(round(time.time() * 1000))
-        secret_enc = secret.encode('utf-8')
-        string_to_sign = '{}\n{}'.format(timestamp, secret)
-        string_to_sign_enc = string_to_sign.encode('utf-8')
-        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
-        sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-        webhook_all = webhook + "&timestamp=" + timestamp + "&sign=" + sign
-        xiaoding = DingtalkChatbot(webhook_all)
-        xiaoding.send_text(msg=os.getenv("REPORT"), at_mobiles=at_phone)
 
-    # @staticmethod
-    # def dingding_suceces():
-    #     webhook = "https://oapi.dingtalk.com/robot/send?access_token=496fea610cae3356bd829c68db5b82b6affe551726c1c917c09c61b7e2114c58"
-    #     send_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-    #     xiaoding = DingtalkChatbot(webhook)
-    #     xiaoding.send_text(msg="""监控情况：无异常\n时间：{}\n类型：小红书数据""".format(send_time), at_mobiles=["18695682863"])
+    @staticmethod
+    def dingding(title):
+        """使用钉钉，发送消息"""
+
+        # 机器人的webhooK
+        webhook = "https://oapi.dingtalk.com/robot/send?access_token=496fea610cae3356bd829c68db5b82b6affe551726c1c917c09c61b7e2114c58"
+        send_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        xiaoding = DingtalkChatbot(webhook)
+        xiaoding.send_text(msg="""监控等级：WARNING\n时间：{}\n类型：小红书数据\n内容：{}""".format(send_time,title), at_mobiles=["17350894220","18521716740"])
+
+    @staticmethod
+    def dingding_suceces():
+        webhook = "https://oapi.dingtalk.com/robot/send?access_token=496fea610cae3356bd829c68db5b82b6affe551726c1c917c09c61b7e2114c58"
+        send_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        xiaoding = DingtalkChatbot(webhook)
+        xiaoding.send_text(msg="""监控情况：无异常\n时间：{}\n类型：小红书数据""".format(send_time), at_mobiles=["18695682863"])
 
 
     @staticmethod
